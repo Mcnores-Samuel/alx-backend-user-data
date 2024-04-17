8b19e95bb1c5abaf64eed786df9f37f521f2c3f2
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-04-17 04:40:31 +0200

    fixed pycodestyle error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized', strict_slashes=False, methods=['GET'])
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:

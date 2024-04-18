3efff86d8c7a995afe3e328907768313b6d94ce4
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-04-19 01:09:41 +0200

    returns the user email and password from the Base64 decoded value.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -36,3 +36,18 @@ class BasicAuth(Auth):
         except Exception:
             return None
         return credentials
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """returns the user email and password from the
+        Base64 decoded value.
+        """
+        if decoded_base64_authorization_header is None:
+            return(None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ':' in decoded_base64_authorization_header:
+            credentials = decoded_base64_authorization_header.split(":")
+            return (credentials[0], credentials[1])
+        return (None, None)

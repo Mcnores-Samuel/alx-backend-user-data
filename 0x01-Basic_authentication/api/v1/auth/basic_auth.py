6ea26869560259e32a9ed128325c7d737bdf1478
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-04-18 05:13:05 +0200

    adding Basic auth mechanism for authorising users

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+"""This module contains the basic authentication logic for the API"""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class to manage the API authentication"""
+    pass

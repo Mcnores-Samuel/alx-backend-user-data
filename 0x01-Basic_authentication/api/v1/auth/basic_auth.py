5dcbe5a4039e8134ba0bd8edc4210498a198c72a
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-04-19 00:35:13 +0200

    returns the decoded value of a Base64 string base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """This module contains the basic authentication logic for the API"""
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,21 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """Returns the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return
+        if not isinstance(base64_authorization_header, str):
+            return
+        try:
+            credentials = base64_authorization_header.encode('utf-8')
+            credentials = b64decode(credentials)
+            credentials = credentials.decode('utf-8')
+        except Exception:
+            return None
+        return credentials

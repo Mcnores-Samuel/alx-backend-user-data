ab4120301546ef4758ed7988291bfd7f000aba2f
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-04-25 01:14:21 +0200

    adding new user and returning all user objcts

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -33,6 +33,6 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Creates a new user object and returns the user objects"""
         user = User(email=email, hashed_password=hashed_password)
-        self.__session.add(user)
-        self.__session.commit()
+        self._session.add(user)
+        self._session.commit()
         return user

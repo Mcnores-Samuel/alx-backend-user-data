c14668d1dbcd1a7e3dca791aad159d7e5c6185af
Mcnores-Samuel <samuelmcnores1@gmail.com>
2024-04-25 14:54:14 +0200

    This method takes in arbitrary keyword arguments and returns the first row found in the users table as filtered by the method’s input arguments

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -36,3 +38,18 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ This method takes in arbitrary keyword arguments and
+        returns the first row found in the users table as filtered
+        by the method’s input arguments
+        """
+        schema = ['id', 'email', 'hashed_password',
+                  'session_id', 'reset_token']
+        form key in kwargs.keys():
+            if key not in schema:
+                raise InvalidRequestError
+        query = self._session(User).filter(**kwargs).first()
+        if query is None:
+            raise NoResultFound
+        return query
